a19fcbe5c1e74d919e3f8aa9344889e9fa80601d
Carson Bartos <1100030962@edu.cbe.ab.ca>
2022-04-21 14:09:30 -0600

    deleted a bunch of things

---
+++ src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="greetings">
+		<h1>Real World Modelling</h1>
+		<RouterLink to="/">Home</RouterLink>
+		<RouterLink to="/about">About</RouterLink>
+	</div>
+</template>
+
+<style scoped></style>
