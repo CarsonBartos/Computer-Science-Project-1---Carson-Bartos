2c35d0b778f9e43b77679da4e7be7d439a628f24
Carson Bartos <1100030962@edu.cbe.ab.ca>
2022-05-10 12:53:25 -0600

    stuff

--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -17,59 +17,65 @@
 		</div>
 
 		<br />
+		<div id="graphic">
+			<h3>Graphic Design</h3>
 
-		<h3>Graphic Design</h3>
-
-		<p>
-			Graphic design isn’t just funky art and sweet designs, it’s seriously the
-			digital medium for a lot of creative expression. While we might only see
-			the final end product of graphic art projects, there’s an infinite number
-			of applicable uses for graphic digital expression. The foundational
-			application of marketing and strategy is the logo design. Often recognizes
-			as the iconic part of a brand, a strong logo speaks volumes to your
-			audience, going beyond the actual imprint and what feelings, emotions and
-			thoughts it instills in it’s audience.Graphic art, advertising, print
-			media, digital ads… all are reated, modified or edited via graphic means.
-			A solid application and production of high quality graphic design tells
-			your audience about your brand and the type of company you are.
-		</p>
+			<p>
+				Graphic design isn’t just funky art and sweet designs, it’s seriously
+				the digital medium for a lot of creative expression. While we might only
+				see the final end product of graphic art projects, there’s an infinite
+				number of applicable uses for graphic digital expression. The
+				foundational application of marketing and strategy is the logo design.
+				Often recognizes as the iconic part of a brand, a strong logo speaks
+				volumes to your audience, going beyond the actual imprint and what
+				feelings, emotions and thoughts it instills in it’s audience.Graphic
+				art, advertising, print media, digital ads… all are reated, modified or
+				edited via graphic means. A solid application and production of high
+				quality graphic design tells your audience about your brand and the type
+				of company you are.
+			</p>
+		</div>
 		<br />
 
-		<h3>3D & Animation</h3>
+		<div id="anim">
+			<h3>3D & Animation</h3>
 
-		<p>
-			We design a wide variety of animations for websites and business. With
-			professional voice-overs, 3D experts and savvy artists, we draw on a wide
-			variety of resources to produce high quality video animation services. Our
-			team can create short clips, promotional trailers or information based
-			explainers, all which helps you reach your audience in a more efficient
-			fashion. Our skilled animators and designers also produce high quality 3D
-			stills, renders and isolated objects for professional, creative and
-			personal uses.
-		</p>
+			<p>
+				We design a wide variety of animations for websites and business. With
+				professional voice-overs, 3D experts and savvy artists, we draw on a
+				wide variety of resources to produce high quality video animation
+				services. Our team can create short clips, promotional trailers or
+				information based explainers, all which helps you reach your audience in
+				a more efficient fashion. Our skilled animators and designers also
+				produce high quality 3D stills, renders and isolated objects for
+				professional, creative and personal uses.
+			</p>
+		</div>
 		<br />
 
-		<h3>Photography</h3>
+		<div id="photo">
+			<h3>Photography</h3>
 
-		<p>
-			There’s a reason why photography can be a fulltime job, someone’s life
-			work and the driver behind the world’s most successful social media
-			platforms. Photos speak louder than words. In one moment, an image can
-			tell you more than an entire thesis. Imagery is Powerful We know design,
-			marketing and advertising play critical roles in communications, but good
-			photography makes the planning, research and production of these
-			strategies possible. Whether it be people, food, furniture or design, the
-			difference in lighting, aperture, exposure and setting can make the
-			difference between okay and incredible.
-		</p>
-		<p>
-			Our team makes sure we deliver the best photographic services to make your
-			brand click. Combined with digital enhancement, graphic design and
-			attention to detail, we make sure that the ultimate end product in any
-			campaign captures, the hearts, minds and eyes of onlookers.
-		</p>
+			<p>
+				There’s a reason why photography can be a fulltime job, someone’s life
+				work and the driver behind the world’s most successful social media
+				platforms. Photos speak louder than words. In one moment, an image can
+				tell you more than an entire thesis. Imagery is Powerful We know design,
+				marketing and advertising play critical roles in communications, but
+				good photography makes the planning, research and production of these
+				strategies possible. Whether it be people, food, furniture or design,
+				the difference in lighting, aperture, exposure and setting can make the
+				difference between okay and incredible.
+			</p>
+			<p>
+				Our team makes sure we deliver the best photographic services to make
+				your brand click. Combined with digital enhancement, graphic design and
+				attention to detail, we make sure that the ultimate end product in any
+				campaign captures, the hearts, minds and eyes of onlookers.
+			</p>
+		</div>
 		<br />
-		<div id="dprinting">
+		<div id="printing">
 			<h3>3D Printing /RAPID PROTOTYPING</h3>
 
 			<p>
@@ -93,7 +99,7 @@ export default {};
 	flex-direction: column;
 	align-items: center;
 }
-#dprinting {
+#printing {
 	background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
 		url("@/assets/il_794xN.2364984458_hsct (1).webp");
 	width: 750px;
@@ -112,6 +118,10 @@ export default {};
 	flex-direction: column;
 	justify-content: center;
 	align-content: center;
+}
+#anim {
+}
+#photo {
 }
 h3 {
 	text-align: center;
