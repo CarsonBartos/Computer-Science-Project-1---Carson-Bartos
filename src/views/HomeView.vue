5d30da7e2ca7e163df0f1af2e6002f7849b487ca
Carbar14 <csbartos@icloud.com>
2022-05-13 22:22:10 -0600

    Review stuff

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,147 @@
 <template>
-	<h1>HOMMMMEEEE</h1>
+<div id="titleCont">
+
+	<h2 id="title">HOME</h2>
+</div>
+	<div id="reviews">
+		<div id="reviewsTitle">
+
+			Here are some testimonials from some happy customers!
+		</div>
+<div class="review">
+	<div class="name">
+		
+		Crystal
+	</div>
+	★★★★★5/5
+	<p id="quote">
+
+		"Parts received. Quality is fantastic! Thank you for getting them all done so quickly. It makes a big difference to us this end."
+	</p>
+
+</div>
+<div class="review">
+	<div class="name">
+		
+		Travis
+	</div>
+	★★★3/5
+	<p id="quote">
+
+		"Real World Modelling are the most communicative and helpful prototyping bureau out there. They delivered our fine detailed parts in record time.  One was ruined though."
+	</p>
+
+</div>
+<div class="review">
+	<div class="name">
+		
+		Donna
+	</div>
+	★★★★★5/5
+	<p id="quote">
+
+		"We have just received the two components with the text and they are great, thank you very much for taking the time and effort to get this right for us, I really appreciate the lengths you have gone to and will recommend your services at every opportunity."
+	</p>
+	
+</div>
+<div class="review">
+	<div class="name">
+		
+		Jonathan 
+	Local Business Owner of Weststar Doll Makers Edmonton Alberta
+	</div>
+
+	★★★★★5/5
+	<p id="quote">
+
+		"The service was second to none. Fast response with a quote. Follow up emails keeping me informed of order progress and delivered exactly on time. Great quality of parts created from my cad drawings.....A great company to deal with. Quick and efficient service with great communication at each stage of the process. I highly recommend them and I would definitely use them again. It's the way all companies should operate."
+	</p>
+</div>
+<div class="review">
+	<div class="name">
+		
+		Jim
+	</div>
+	★★★★★5/5
+	<p id="quote">
+
+		"I have worked in the Formula 1 sector for 20 years and had various in-house equipment to manufacture demo and sample goods but having left to start my own venture I found Real World Modelling a great option to get my sample goods made quickly and efficiently. " 🙂
+	</p>
+
+</div>
+<div class="review">
+	<div class="name">
+		
+		Sally
+	</div>
+	★★★★★5/5
+	<p id="quote">
+
+		"A good service, quick and accurate, with friendly help in resolving problems, and trust me, I have lots."
+	</p>
+
+</div>
+<div class="review">
+	<div class="name">
+		
+		Sherry
+	</div>
+	★★★★★5/5
+	<p id="quote">
+
+		"Got the part, it's absolutely beautiful, you guys are super-awesome!"
+	</p>
+
+</div>
+<div class="review">
+	<div class="name">
+		
+		Numa
+	</div>
+	★★★★★5/5
+	<p id="quote">
+
+		"Very quick, helpful with error trapping on print design. A good service, quick and accurate, with friendly help in resolving problems."
+	</p>
+
+</div>
+<div class="review">
+	<div class="name">
+
+		Jacquie
+	</div>
+	★★★★★5/5
+	<p id="quote">
+
+		"Very helpful with advice pre printing very prompt manufacture and delivery."
+	</p>
+</div>
+<div class="review">
+<div class="name">
+		
+	Stu
+	</div>
+	★★★★4/5
+	<p id="quote">
+
+		"Just wanted to inform that we received our order a couple of days ago and all seems to be in order. Part and detail quality is good and we couldn't find any defects.
+	We were particularly impressed with your Polypropylene -like material, Endur. It's much friendlier for simulation of plastic parts and snaps than other materials we worked with before."
+	</p>
+
+</div>
+
+	</div>
 </template>
+
+<style scoped>
+ .review{
+	 padding: 10px
+ }
+ #reviewsTitle{
+	 text-align: center;
+	 font-size: 30px;
+ }
+ .name{
+	 font-size: 20px;
+ }
+</style>
